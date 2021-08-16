ca8a21bc15e7cfc1725f066e18f25a2dc644d04d
ajh123 <samrosoft36@gmail.com>
2021-08-16 17:20:15 +0100

    Begin implemenation of /ta claim_plot

--- src/main/java/towny_extras/towny_extras/commands/PlotClaimCommand.java
+++ src/main/java/towny_extras/towny_extras/commands/PlotClaimCommand.java
@@ -1,13 +1,52 @@
 package towny_extras.towny_extras.commands;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
+import com.palmergames.bukkit.towny.TownyAPI;
+import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
+import com.palmergames.bukkit.towny.object.Town;
+import com.palmergames.bukkit.towny.object.TownBlock;
+import org.bukkit.Location;
+import org.bukkit.command.*;
+import org.bukkit.entity.Player;
+
+import java.util.Arrays;
 
 public class PlotClaimCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        sender.sendMessage("Claim plot");
+        if (args[0].equals("claim_plot")){
+            try {
+                Location loc = null;
+                Town town = TownyAPI.getInstance().getDataSource().getTown(args[1]);
+                if(sender instanceof ConsoleCommandSender || sender instanceof RemoteConsoleCommandSender){
+                    //TODO: stuff here
+                }
+                if(sender instanceof Player){
+                    Player player = (Player) sender;
+                    loc = player.getLocation();
+                }
+                if(loc == null){
+                    sender.sendMessage("Location is invalid (null)");
+                    return false;
+                }
+                TownBlock block = TownyAPI.getInstance().getTownBlock(loc);
+                if(block == null){
+                    sender.sendMessage("Location has not been registered");
+                    return false;
+                }
+                if(block.hasTown()){
+                    Town blockTown = block.getTownOrNull();
+                    if(blockTown == null){
+                        sender.sendMessage("Location is not part of town");
+                        return false;
+                    }
+                    sender.sendMessage("Location is part of "+blockTown.getName());
+                    return false;
+                }
+            } catch (NotRegisteredException e) {
+                sender.sendMessage(e.getMessage());
+                return false;
+            }
+        }
         return true;
     }
 }
