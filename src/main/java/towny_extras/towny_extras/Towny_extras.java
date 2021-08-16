4bb9f0c536eb17940e658f0335b89f8cb7699bd0
ajh123 <samrosoft36@gmail.com>
2021-08-16 13:46:13 +0100

    Added source

---
+++ src/main/java/towny_extras/towny_extras/Towny_extras.java
@@ -0,0 +1,17 @@
+package towny_extras.towny_extras;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Towny_extras extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
